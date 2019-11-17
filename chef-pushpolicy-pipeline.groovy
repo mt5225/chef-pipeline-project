7ececaa21870b9d507208ed9c02212430fdfeab5
Qing Jiang <qing.j@qingj-m1551.lan>
2019-11-16 16:43:14 -0800

    update

--- chef-pushpolicy-pipeline.groovy
+++ chef-pushpolicy-pipeline.groovy
@@ -5,6 +5,10 @@ pipeline {
         }
     }
 
+    environment {
+		CHEF_LICENSE = 'accept'
+	}
+
     // triggers {
     //     pollSCM 'H/5 * * * *'
     // }
@@ -12,7 +16,7 @@ pipeline {
     parameters {
         choice choices: ['DEV', 'QA', 'PROD'], description: '', name: 'POLICY_GROUP'
         choice choices: ['petstorechef'], description: '', name: 'COOKBOOK'
-        choice choices: ['','petstore-biserver', 'petstore-webserver'], description: '', name: 'POLICY_NAME'
+        choice choices: ['petstore-webserver','petstore-biserver'], description: '', name: 'POLICY_NAME'
         string defaultValue: '-1', description: '', name: 'BUILD_REVISION', trim: true
     }
 
